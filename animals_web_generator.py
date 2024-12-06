b5ffff6b883d3b195a1e13bef9d0304d65728970
Jan-Philipp Gutt <janphilippgutt5000@gmail.com>
2024-12-06 19:07:41 +0100

    animals added to html template

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,6 +7,7 @@ def load_data(file_path):
 
 animals_data = load_data('animals_data.json')
 
+'''
 for animal in animals_data:
       print(f"Name: {animal["name"]}\nDiet: {animal["characteristics"]["diet"]}\n"
             f"Location: {animal["locations"][0]}")
@@ -15,3 +16,25 @@ for animal in animals_data:
       except KeyError:
         print("")
         continue
+'''
+
+with open("animals_template.html", "r") as fileobj:
+    text_html = fileobj.read()
+
+output = ''  # define an empty string
+for animal in animals_data:
+    # append information to each string
+    output += f"Name: {animal["name"]}\n"
+    output += f"Diet: {animal["characteristics"]["diet"]}\n"
+    output += f"Location: {animal["locations"][0]}\n"
+    try:
+        output += f"Type: {animal["characteristics"]["type"]}\n\n"
+    except KeyError:
+        output += f"\n"
+        continue
+
+#print(output)
+
+new_text = text_html.replace("__REPLACE_ANIMALS_INFO__", output)
+
+print(new_text)
