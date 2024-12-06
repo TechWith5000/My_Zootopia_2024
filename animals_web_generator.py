a6bd2e8f2102babbccdcfeb57697b436fef30283
Jan-Philipp Gutt <janphilippgutt5000@gmail.com>
2024-12-06 19:53:21 +0100

    clean .py file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,38 +7,30 @@ def load_data(file_path):
 
 animals_data = load_data('animals_data.json')
 
-'''
-for animal in animals_data:
-      print(f"Name: {animal["name"]}\nDiet: {animal["characteristics"]["diet"]}\n"
-            f"Location: {animal["locations"][0]}")
-      try:
-          print(f"Type: {animal["characteristics"]["type"]}\n")
-      except KeyError:
-        print("")
-        continue
-'''
 
 with open("animals_template.html", "r") as fileobj:
     text_html = fileobj.read()
 
-output = ''  # define an empty string
-for animal in animals_data:
+
+def serialize_animal(animal_obj):
+    output = ''  # define an empty string
     # append information to each string
     output += '<li class="cards__item">'
-    output += f"<div class='card__title'>{animal["name"]}</div><p class='card__text'>\n"
-    output += f"<strong>Diet:</strong>{animal["characteristics"]["diet"]}<br/>\n"
-    output += f"<strong>Location:</strong>{animal["locations"][0]}<br/>\n"
+    output += f"<div class='card__title'>{animal_obj["name"]}</div><p class='card__text'>\n"
+    output += f"<strong>Diet:</strong>{animal_obj["characteristics"]["diet"]}<br/>\n"
+    output += f"<strong>Location:</strong>{animal_obj["locations"][0]}<br/>\n"
     try:
-        output += f"<strong>Type:</strong>{animal["characteristics"]["type"]}<br/>\n\n</p></li>"
+        output += f"<strong>Type:</strong>{animal_obj["characteristics"]["type"]}<br/>\n\n</p></li>"
     except KeyError:
         output += f"\n</p></li>"
-        continue
+    return output
 
-#print(output)
+output = ''
+for animal_obj in animals_data:
+    output += serialize_animal(animal_obj)
 
 new_text = text_html.replace("__REPLACE_ANIMALS_INFO__", output)
 
-print(new_text)
 
 with open("animals.html", "w") as fileobj:
     fileobj.write(new_text)
