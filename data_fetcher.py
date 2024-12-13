7bc15711348b8eac31dcc82e2865b3d6bd7beacf
Jan-Philipp Gutt <janphilippgutt5000@gmail.com>
2024-12-13 14:52:21 +0100

    updated constants and indentation in data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,28 +1,28 @@
 import requests
 
+API_KEY = "PB3HeskWDOYsKFXahX/0XQ==CKWeyxWZg6YfZN0d"
 def fetch_data(animal_name):
-  """
-  Fetches the animals data for the animal 'animal_name'.
-  Returns: a list of animals, each animal is a dictionary:
-  {
-    'name': ...,
-    'taxonomy': {
-      ...
-    },
-    'locations': [
-      ...
-    ],
-    'characteristics': {
-      ...
-    }
-  },
-  """
-  url = "https://api.api-ninjas.com/v1/animals?name=" + animal_name
-  api_key = "PB3HeskWDOYsKFXahX/0XQ==CKWeyxWZg6YfZN0d"
-  headers = {
-      "X-Api-Key": api_key
-  }
-  response = requests.get(url, headers=headers)
-  response = response.json()
-  return response
+      """
+      Fetches the animals data for the animal 'animal_name'.
+      Returns: a list of animals, each animal is a dictionary:
+      {
+        'name': ...,
+        'taxonomy': {
+          ...
+        },
+        'locations': [
+          ...
+        ],
+        'characteristics': {
+          ...
+        }
+      },
+      """
+      url = "https://api.api-ninjas.com/v1/animals?name=" + animal_name
+      headers = {
+          "X-Api-Key": API_KEY
+      }
+      response = requests.get(url, headers=headers)
+      response = response.json()
+      return response
 
